f0909cacd52bb7ad8a337d57bfe011eb7e6ffda0
DLkon <thedaniellopes@gmail.com>
2026-05-03 18:19:07 -0300

    new logic and UI

--- script.js
+++ script.js
@@ -1,133 +1,128 @@
-'use strict'
 
-let first;
-let second;
-let array = [];
-const game = (() => { 
-    let board = ["","","","","","","","",""];
-    let back = () => {
-        const container = document.querySelector('.game');
-        const section = document.querySelector('.info');
-        container.style.display = "none";
-        section.style.display = "flex";  
-    };
+let playerX, playerO, playerTurn; 
+const cells = document.querySelectorAll('.cell');
+const statusSpan = document.querySelector('.status span');
+const resetBtn = document.getElementById('reset-btn');
+playerO = 'O';
+playerX = 'X';
+playerTurn = 'X';
+let moveCounter = 0;
+let gameBoard = [
+      ['','',''],
+      ['','',''],
+      ['','','']
+];
 
-    let start = () => {
-        const container = document.querySelector('.game');
-        const section = document.querySelector('.info');
-        container.style.display = "flex";
-        section.style.display = "none";    
-        first = player(document.getElementById('Player1').value,1,"X");
-        second = player(document.getElementById('Player2').value,2,"O")      
-    };
+
+
+cells.forEach(cell => {
+    cell.addEventListener('click',  () => {
+        
     
-    let restart = () => { 
-    const btn = document.querySelectorAll('button');    
-    const div = document.querySelector('.textBox');
-    div.textContent = '';
-    btn.forEach(function(button){
+        if (playerTurn == playerX) {
 
-    if(button.id == "back"){
-        button.textContent = "Back";
-    }
-    else if (button.id == "restart"){
-        button.textContent = "Restart game";
-    }
-    else if (button.id == "submit"){
-        button.textContent = "Start Game";
-    }
-    else {
-    button.textContent = "";
-    game.board = ["","","","","","","","",""];
-    button.disabled = false;
+            let rowId = cell.getAttribute('data-row');
+            let columnId = cell.getAttribute('data-col');
 
-    };
-});
-};
-    return {board,back,start,restart}
-
-})();
-
-let start = document.querySelector('#submit');
-let back = document.querySelector('.back');
-let restart = document.querySelector('.restart');
-
-const player = (name,position,symbol)  => { 
+            console.log(rowId);
+            console.log(columnId);
+            gameBoard[rowId][columnId] = playerTurn;
+            cell.classList.add(playerTurn.toLowerCase());
+            
     
-    return {name,position,symbol}
-};
-
-back.addEventListener('click',game.back);
-start.addEventListener('click',game.start);
-restart.addEventListener('click',game.restart);
+            cell.textContent = playerTurn;
+            playerTurn = playerO;
+            statusSpan.textContent = playerTurn;
+            moveCounter++;
 
 
-const checkGame = (() => {
+
+        } else if (playerTurn == playerO) {
+
+            let rowId = cell.getAttribute('data-row');
+            let columnId = cell.getAttribute('data-col');
+
+            console.log(rowId);
+            console.log(columnId);
+             gameBoard[rowId][columnId] = playerTurn;
+            cell.classList.add(playerTurn.toLowerCase());
+            cell.textContent = playerTurn;
+            playerTurn = playerX;
+            statusSpan.textContent = playerTurn;
+            moveCounter++;
+        }
+
+        if(moveCounter >= 5) {
+            let result =  check_Game();
+            end_game(result);
+            console.log('result' + result);
+
+        }
    
-    const win = () => {
-        if  (game.board[0] == "X" && game.board[1] == "X" && game.board[2] == 'X' || game.board[3] == "X"
-        && game.board[4] == "X" && game.board[5]=="X" || game.board[6] == "X" && game.board[7] == "X"
-        && game.board[8] == "X" || game.board[0] == "X" && game.board[3] == "X" && game.board[6] == "X"
-        || game.board[1] == "X" && game.board[4] == "X" && game.board[7] == "X" || game.board[2] == "X"
-        && game.board[5] == "X" && game.board[8] =="X" || game.board[0] == "X" && game.board[4] == "X" &&
-        game.board[8] == "X" || game.board[2] == "X" && game.board[4]=="X" && game.board[6] == "X")
-        {   
-            const btn = document.querySelectorAll('div.container button');
-            const div = document.querySelector('.textBox');
-            div.textContent = first.name + " " + "wins";
-            btn.forEach(function(button){
-                button.disabled = true;
-        });
-    }
-         else if (game.board[0] == "O" && game.board[1] == "O" && game.board[2] == 'O' || game.board[3] == "O"
-        && game.board[4] == "O" && game.board[5]=="O" || game.board[6] == "O" && game.board[7] == "O"
-        && game.board[8] == "O" || game.board[0] == "O" && game.board[3] == "O" && game.board[6] == "O"
-        || game.board[1] == "O" && game.board[4] == "O" && game.board[7] == "O" || game.board[2] == "O"
-        && game.board[5] == "O" && game.board[8] || game.board[0] == "O" && game.board[4] == "O" &&
-        game.board[8] == "O" || game.board[2] == "O" && game.board[4]=="O" && game.board[6] == "O"){
-        const div = document.querySelector('.textBox');    
-        const btn = document.querySelectorAll('div.container button');
-        div.textContent = second.name + " " + "wins";
-        btn.forEach(function(button){
-            button.disabled = true;
-        });
-    } 
-    else if (array.length == 9) {
-        const div = document.querySelector('.textBox');
-        div.textContent = "It's a Draw :("
-    }
-        
-};   
 
-  return {win} 
-})();
+    }, { once: true })
+})
 
-const btns = document.querySelectorAll('div.container button');
-btns.forEach(function(button){
-    button.addEventListener('click',()=>{
-       let square = button.id;
-        if (first.position == 1) {
-        array.push(first.symbol);
-        document.getElementById(square).textContent = "X"
-        let btn = document.getElementById(square);
-        let num = btn.dataset.number;
-        game.board.splice(num,1,first.symbol)
-        first.position = 0;
-        button.disabled = true;
-        checkGame.win(); 
-        }
-        
-        else {
-        document.getElementById(square).textContent = "O"
-        let btn = document.getElementById(square);
-        let num = btn.dataset.number;
-        array.push(second.symbol);
-        game.board.splice(num,1,second.symbol)
-        first.position = 1;
-        button.disabled = true;
-        checkGame.win();
+
+function check_diagonals(){
+  let primary = [];
+  let secondary = [];
+  let j = 2;
+  for(n = 0; n <= 2; n++){ 
+    primary.push(gameBoard[n][n]);
+    secondary.push(gameBoard[n][j]);
+    j = j -1;
+  }
+  
+  return [primary, secondary];
+}
+
+function check_Game() {
+
+    let thereIsWinner = false;
+    let col;
+    diagonals = check_diagonals();
+    
+    console.log(gameBoard);
+    for (let n = 0; n <= 2; n++) { 
+            //checking rows
+            console.log('n' + n);
+        if( gameBoard[n].every(move => move != null && move == gameBoard[n][0])){
+            thereIsWinner = true;
+            return gameBoard[n][0];
+            //checking columns 
+        } else {
+            col = gameBoard.map(row => row[n]);
+            if(col.every(move => move != null && move == col[n])) {
+                thereIsWinner = true;
+                return col[n];
+            }
         }
 
-    });
-});
+    }
 
+    diagonals = check_diagonals();
+    
+    for(let n = 0; n <=1; n++) {
+        console.log('hi');
+        if(diagonals[n].every(move => move != null && move == diagonals[n][0])){
+            return diagonals[n][0];
+        }
+    }
+
+    
+}
+
+function end_game(result) {
+       if (result == playerO) {
+                console.log('hi o ');
+                statusSpan.textContent = `${playerO} Wins!`;
+                document.getElementById('board').style.pointerEvents = 'none';
+
+        } else if (result == playerX) {
+                console.log('hi x ');
+                statusSpan.textContent = `${[playerX]} Wins!`;
+                document.getElementById('board').style.pointerEvents = 'none';
+            }
+
+}
